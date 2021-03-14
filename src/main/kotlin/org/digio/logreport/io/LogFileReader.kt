c67e2a624370fbaefc296c6b44e2c32c4ab7f563
divinpaul <divinpaul@gmail.com>
2021-03-14 13:09:46 +1100

    Move Regex Patterns outside of the loop to avoid unnecessary object creation.

--- src/main/kotlin/org/digio/logreport/io/LogFileReader.kt
+++ src/main/kotlin/org/digio/logreport/io/LogFileReader.kt
@@ -6,14 +6,18 @@ private const val IP_REGEX = """\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"""
 private const val URL_REGEX = """(?<=GET\s|POST\s|PUT\s|PATCH\s|DELETE\s)(.*)(?=\s+HTTP)"""
 
 class LogFileReader : InputFileReader {
-    override fun read(inputFile: File) =
-        inputFile
+    override fun read(inputFile: File): List<Pair<String, String>> {
+        val ipRegexPattern = IP_REGEX.toRegex()
+        val urlRegexPattern = URL_REGEX.toRegex()
+
+        return inputFile
             .useLines { line -> removeBlankLines(line).toList() }.map { line ->
-                val ipAddress = IP_REGEX.toRegex().find(line)?.value ?: ""
-                val url = URL_REGEX.toRegex().find(line)?.value ?: ""
+                val ipAddress = ipRegexPattern.find(line)?.value ?: ""
+                val url = urlRegexPattern.find(line)?.value ?: ""
                 ipAddress to url
             }
             .filterNot { it.first.isBlank() || it.second.isBlank() }
+    }
 
     private fun removeBlankLines(lines: Sequence<String>) = lines.filter { it.isNotEmpty() }
 }
