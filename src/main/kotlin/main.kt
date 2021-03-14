1ad71f64c8090e32afe6686f8040b6e12381620c
divinpaul <divinpaul@gmail.com>
2021-03-14 14:30:09 +1100

    integrate the application with main function.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
+import org.digio.logreport.LogReportService
+import org.digio.logreport.io.ConsoleWriter
+import org.digio.logreport.io.LogFileReader
+import java.io.File
+
 fun main(args: Array<String>) {
-    print("hello world")
+    if (args.size != 1) {
+        println("Please provide the input file name. Refer README for details.")
+        return
+    }
+
+    LogReportService(LogFileReader(), ConsoleWriter()).generate(File(args[0]))
 }
